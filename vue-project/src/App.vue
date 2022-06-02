a2864c43bb8347cd8f5e3e82a25896701b4f756f
Faez-B <faezbhatti@gmail.com>
2022-06-02 12:19:57 +0200

    Fin de l'exercice sur les Tableaux

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -5,26 +5,63 @@
   import {ref} from "vue";
   import axios from 'axios';
 
-  const users = ref(null);
+  // const users = ref(null);
 
-  axios.get("http://localhost:8000/api/names")
-  .then( (usersData) => {
-    users.value = usersData.data;
-  })
+  // axios.get("http://localhost:8000/api/names")
+  // .then( (usersData) => {
+  //   users.value = usersData.data;
+  // })
+
+
+  let id = ref(0);
+
+  const users = ref([
+    {id : id.value++, prenom: "Alice", nom: "Smith"},
+    {id : id.value++, prenom: "Bob", nom: "Stark"},
+    {id : id.value++, prenom: "Charlie", nom: "Doe"}
+  ]);
+
+  const prenom = ref("");
+  const nom = ref("");
+
+  function ajouterUser() {
+    if (prenom.value && nom.value) {
+      users.value.push({
+        id: id.value++,
+        prenom : prenom.value,
+        nom : nom.value
+      });
+
+      prenom.value = "";
+      nom.value = "";
+    }
+  }
 
 </script>
 
 <template>
 
   <main>
-    <Ex1_2 />
-    <Ex3_4 />
+    <!-- <Ex1_2 /> -->
+    <!-- <Ex3_4 /> -->
 
-    <h1>Ex Avancé : Lien entre le système d'authentification du backend et celui du frontend </h1>
+    <!-- <h1>Ex Avancé : Lien entre le système d'authentification du backend et celui du frontend </h1> -->
 
-    <p>
+    <!-- <p>
       {{ users }}
-    </p>
+    </p> -->
+
+    <ul>
+      <li v-for="user in users" :key="user.id">
+        {{ user }}
+      </li>
+    </ul>
+
+    <form @submit.prevent="ajouterUser()">
+      <input type="text" placeholder="Prénom" v-model="prenom">
+      <input type="text" placeholder="Nom" v-model="nom">
+      <button>Ajouter</button>
+    </form>
   </main>
 
 </template>
