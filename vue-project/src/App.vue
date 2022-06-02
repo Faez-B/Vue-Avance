372baeaf5f58d2cbdbf5308de210298f7a3fbf47
Faez-B <faezbhatti@gmail.com>
2022-06-02 16:18:31 +0200

    Ajout d'un router dans le projet

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -2,31 +2,32 @@
 
 import {ref} from "vue";
 
-const badPassword = ref(false);
-const onSubmit = () => {
-  badPassword.value = !badPassword.value;
-}
 </script>
 
 <template>
 
-  <main class="container">
-    <form @submit.prevent="onSubmit()">
-      <div class="form-group">
-        <label for="exampleInputEmail1">Email address</label>
-        <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
-        <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
-      </div>
-      <div class="form-group">
-        <label for="exampleInputPassword1" class="form-label" :class="{'text-danger' : badPassword}">Password</label>
-        <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
-      </div>
-      <div class="form-check">
-        <input type="checkbox" class="form-check-input" id="exampleCheck1">
-        <label class="form-check-label" for="exampleCheck1">Check me out</label>
-      </div>
-      <button type="submit" class="btn btn-primary">Submit</button>
-    </form>
+  <main>
+    <nav>
+      <ul>
+        <li><RouterLink to="/">Accueil</RouterLink></li>
+
+        <li>
+          <RouterLink :to="{name:'demovfor'}">Demo v-for</RouterLink>
+        </li>
+
+        <li><RouterLink :to="{name:'compteur'}">Exo compteur</RouterLink></li>
+        <li><RouterLink :to="{name:'jwt'}">Exo jwt</RouterLink></li>
+        <li><RouterLink :to="{name:'form'}">Exo class binding</RouterLink></li>
+
+      </ul>
+
+    </nav>
+
+    <RouterView />
+    <!-- <=> -->
+    <!-- <router-view></router-view> -->
+    <!-- <=> -->
+    <!-- <router-view/> -->
   </main>
 
 </template>
