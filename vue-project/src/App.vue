70ebced5efc3894265bede12b814670c16ec6ec8
Faez-B <faezbhatti@gmail.com>
2022-06-02 15:05:49 +0200

    Factorisation de conditionnal rendering

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -1,32 +1,12 @@
 <script setup>
-  import {ref} from "vue";
-
-  const user = ref(null);
-  const connected = ref(false);
-
-  const connexion = () => {
-    connected.value = true;
-    user.value = {
-      name: "John"
-    };
-  }
-
-  const deconnexion = () => {
-    connected.value = false;
-    user.value = null;
-  }
+  import Condition from './components/Condition.vue';
 
 </script>
 
 <template>
 
   <main>
-
-    <h2 v-if="connected">Hello {{ user.name }}!</h2>
-    <h2 v-else>Hello Anonyme</h2>
-
-    <button v-if="!connected" @click="connexion()">Connexion</button>
-    <button v-else @click="deconnexion()">Déconnexion</button>
+    <Condition />
     
   </main>
 
---
+++ vue-project/src/components/Condition.vue
@@ -0,0 +1,35 @@
+<script setup>
+  import {ref} from "vue";
+
+  const user = ref(null);
+  const connected = ref(false);
+
+  const connexion = () => {
+    connected.value = true;
+    user.value = {
+      name: "John"
+    };
+  }
+
+  const deconnexion = () => {
+    connected.value = false;
+    user.value = null;
+  }
+
+</script>
+
+<template>
+
+  <main>
+
+    <h2 v-if="connected">Hello {{ user.name }}!</h2>
+    <h2 v-else>Hello Anonyme</h2>
+
+    <button v-if="!connected" @click="connexion()">Connexion</button>
+    <button v-else @click="deconnexion()">Déconnexion</button>
+    
+  </main>
+
+</template>
+
+<style></style>
