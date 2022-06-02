c446112f57c8dcc14344a00a654dc3985195707a
Faez-B <faezbhatti@gmail.com>
2022-06-02 14:27:14 +0200

    Factorisation exo sur les Tableaux

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -1,6 +1,7 @@
 <script setup>
   import Ex1_2 from "@/components/Ex1_2.vue";
   import Ex3_4 from "@/components/Ex3_4.vue";
+  import DemoVForVue from "@/components/DemoVFor.vue";
 
   import {ref} from "vue";
   import axios from 'axios';
@@ -11,35 +12,6 @@
   // .then( (usersData) => {
   //   users.value = usersData.data;
   // })
-
-
-  let id = ref(0);
-
-  const users = ref([
-    {id : id.value++, prenom: "Alice", nom: "Smith"},
-    {id : id.value++, prenom: "Bob", nom: "Stark"},
-    {id : id.value++, prenom: "Charlie", nom: "Doe"}
-  ]);
-
-  const prenom = ref("");
-  const nom = ref("");
-
-  function ajouterUser() {
-    if (prenom.value && nom.value) {
-      users.value.push({
-        id: id.value++,
-        prenom : prenom.value.trim(),
-        nom : nom.value.trim()
-      });
-
-      prenom.value = "";
-      nom.value = "";
-    }
-  }
-
-  function deleteUser(id) {
-    users.value = users.value.filter( (user) => user.id != id );
-  }
 
 </script>
 
@@ -55,18 +27,7 @@
       {{ users }}
     </p> -->
 
-    <ul>
-      <li v-for="user in users" :key="user.id">
-        {{ user }}
-        <button @click="deleteUser(user.id)">Supprimer</button>
-      </li>
-    </ul>
-
-    <form @submit.prevent="ajouterUser()">
-      <input type="text" placeholder="Prénom" v-model="prenom">
-      <input type="text" placeholder="Nom" v-model="nom">
-      <button>Ajouter</button>
-    </form>
+    <DemoVForVue />
   </main>
 
 </template>
